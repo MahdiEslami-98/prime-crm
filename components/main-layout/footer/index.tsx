98817deeab8f504481fa89a10b2e6b561158332d
Mahdi Eslami(L) <eslami.mahdi.98@gmail.com>
2024-05-19 17:07:48 +0330

    fix: bg color

--- components/main-layout/footer/index.tsx
+++ components/main-layout/footer/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Logo from "@/components/logo";
 import Link from "next/link";
-import { title } from "process";
+import { FaAngleUp } from "react-icons/fa6";
 import React from "react";
 
 const icons = [
@@ -84,7 +84,7 @@ const contact = {
 
 const FooterComponent = () => {
   return (
-    <div className="bg-secondry-06 font-jost">
+    <div className="bg-primary-03 font-jost">
       <div className="container mx-auto px-4 pt-6 md:pt-[124.75px]">
         <div className="block justify-between pb-[39.25px] md:flex">
           <Logo className="text-white" />
@@ -94,7 +94,7 @@ const FooterComponent = () => {
             ))}
           </div>
         </div>
-        <div className="text-white grid grid-cols-2 gap-y-3 pb-[57px] md:grid-cols-3 xl:grid-cols-5 ">
+        <div className="grid grid-cols-2 gap-y-3 pb-[57px] text-white md:grid-cols-3 xl:grid-cols-5 ">
           {links.map((item, index) => (
             <div className="" key={index}>
               <div className="border-y border-[#6c6c6c] py-[17px] text-head5 font-medium">
@@ -114,13 +114,13 @@ const FooterComponent = () => {
               Contact Us
             </div>
             <div className="mr-[41px] pt-[48px]">
-              <div className="from-transparent via-secondry-06 mb-[22px] rounded-[20px] bg-gradient-to-r to-[#6c6c6c] p-px">
-                <div className="bg-secondry-06 rounded-[20px] px-[21px] py-[19px] text-head6">
+              <div className="mb-[22px] rounded-[20px] bg-gradient-to-r from-primary-03 to-[#6c6c6c] p-px">
+                <div className="rounded-[20px] bg-primary-03 px-[21px] py-[19px] text-head6">
                   {contact.address}
                 </div>
               </div>
-              <div className="from-transparent via-secondry-06 rounded-[20px] bg-gradient-to-r to-[#6c6c6c] p-px">
-                <div className="bg-secondry-06 rounded-[20px] px-[21px] py-[19px] text-head6">
+              <div className="rounded-[20px] bg-gradient-to-r from-primary-03 to-[#6c6c6c] p-px">
+                <div className="rounded-[20px] bg-primary-03 px-[21px] py-[19px] text-head6">
                   <p>{contact.phone}</p>
                   <p>{contact.email}</p>
                 </div>
@@ -128,18 +128,21 @@ const FooterComponent = () => {
             </div>
           </div>
           <div className="col-span-2 md:col-span-3 lg:col-span-3 xl:col-span-2">
-            <div className=" border-y border-[#6c6c6c] py-[17px] text-head5 font-medium">
-              Our Latest News
+            <div className=" flex items-center justify-between border-y border-[#6c6c6c] py-[17px] text-head5 font-medium">
+              <span>Our Latest News</span>
+              <div className="flex h-[37px] w-[37px] items-center justify-center rounded-full border border-[#6c6c6c] text-[16px]">
+                <FaAngleUp />
+              </div>
             </div>
             <div className="relative mt-[48px]">
               <img src="/images/Image.png" className="w-full" alt="" />
-              <div className="bg-white/50 text-black absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[5px] px-[44px] py-[14px] text-center text-head6 font-semibold">
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[5px] bg-white/50 px-[44px] py-[14px] text-center text-head6 font-semibold text-black">
                 Read More
               </div>
             </div>
           </div>
         </div>
-        <div className="text-white border-t border-[] py-[27px] text-center text-head6">
+        <div className="border-t border-[#6c6c6c] py-[27px] text-center text-head6 text-white">
           <p>Copyright © Primarly | Designed by VictorFlow Templates </p>
         </div>
       </div>

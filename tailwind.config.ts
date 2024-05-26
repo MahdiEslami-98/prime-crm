52e82f62248aa0ac922407c57caa372eeabc8cd8
Mahdi Eslami(L) <eslami.mahdi.98@gmail.com>
2024-05-26 14:39:19 +0330

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     colors: {
       "primary-01": "#49deff",

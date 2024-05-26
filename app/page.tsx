d8f706cf81751a5b6e830b3d84c905185aa2d73b
Mahdi Eslami(L) <eslami.mahdi.98@gmail.com>
2024-05-26 14:03:51 +0330

    update: pakage section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable @next/next/no-img-element */
 import ExploreSection from "@/components/exploreSection";
 import HeroSection from "@/components/hero";
 import HowItWorkSection from "@/components/howWorkSection";
 import MarketingSection from "@/components/marketing";
+import PakagePriceSection from "@/components/pakagePriceSection";
 import SaleModuleSection from "@/components/saleModuleSection";
 import SliderSection from "@/components/slidersection";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <HeroSection />
@@ -15,26 +15,9 @@ export default function Home() {
       <SaleModuleSection />
       <HowItWorkSection />
       <SliderSection />
-      <div className="container pt-[152px]">
-        <div className="flex flex-col items-center gap-y-[36px] text-primary-03">
-          <div className="max-w-[871px] text-center">
-            <h3 className="font-jost text-head2 font-semibold">
-              Deliver End-To-End Retail Billing & Solutions
-            </h3>
-            <p className="font-open-sans text-head5 text-[#4e4e4e]">
-              Unique and powerful suite of software to run your entire business,
-              brought to you by a company with the long term vision to transform
-              the way you work.
-            </p>
-          </div>
-          <div className="scribe_section h-[533px] w-full rounded-[30px]">
-            <div className="px-[95px] pt-[118px]">
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <PakagePriceSection />
     </>
   );
-}
+};
+
+export default Home;

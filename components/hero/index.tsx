dfbc540a85709ddb4e28eabd4acb60a664933880
Mahdi Eslami(L) <eslami.mahdi.98@gmail.com>
2024-05-27 12:12:39 +0330

    update: add animation to components

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,6 +1,8 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Button from "../button";
+import { m, motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
@@ -9,30 +11,42 @@ const HeroSection = () => {
         <img src="/images/abs-top.png" alt="" />
       </div>
       <div className="relative border-b border-[#dedede]">
-        <div className="absolute left-[32%] top-[65%]">
+        <motion.div
+          transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+          animate={{ rotate: 360, x: [-60, 60, -60] }}
+          className="absolute left-[32%] top-[65%]"
+        >
           <img src="/images/tri1.png" alt="" />
-        </div>
-        <div className="absolute left-[28%] top-[10%]">
+        </motion.div>
+        <motion.div
+          transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+          animate={{ rotate: [180, -180, 180], x: [-2, 2, -2], y: [-6, 3, -5] }}
+          className="absolute left-[28%] top-[10%]"
+        >
           <img src="/images/tri2.png" alt="" />
-        </div>
-        <div className="absolute -left-[12%] top-0 animate-spin3">
+        </motion.div>
+        <div className="absolute -left-[12%] top-0 animate-spin">
           <img src="/images/p1.png" alt="" />
         </div>
-        <div className="absolute left-[40%] top-[40%] animate-spin2">
+        <div className="absolute left-[40%] top-[40%] animate-spin">
           <img src="/images/p2.png" alt="" />
         </div>
         <div className="absolute right-0 top-[15%] z-20 animate-pulse">
           <img src="/images/Ellipse 277.png" alt="" />
         </div>
 
-        <div className="absolute right-[2%] top-[18%]  hidden animate-spin md:block">
+        <div className="absolute right-[2%] top-[18%]  hidden animate-spin2 md:block">
           <img src="/images/Group 2485.png" alt="" />
         </div>
         <div className="absolute -left-[10%] top-[62%] z-10 hidden animate-pulse md:block">
           <img src="/images/Ellipse 276.png" alt="" />
         </div>
         <div className="broder-b relative grid grid-cols-hero items-center border-[#dedede] pb-[56.21px] pt-[185px]">
-          <div className="">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+          >
             <div className="mb-[34px]">
               <h1 className="pb-[21px] pr-[49px] font-jost text-head1 font-semibold">
                 The CRM Platform Your Whole Business Will Love
@@ -67,23 +81,53 @@ const HeroSection = () => {
               </div>
               <img src="/images/star2.png" alt="" />
             </div>
-          </div>
+          </motion.div>
           <div className="relative z-20 hidden h-full xl:block">
-            <div className="absolute -top-[10%] left-[4%] h-[198.06px] w-[367.09px] rotate-[-21deg] rounded-[35px] shadow-xl">
-              <img src="/images/e1.png" alt="" className="h-full w-full" />
-            </div>
-            <div className="absolute left-[19%] top-[18%] h-[339.09px] w-[305.12px] rotate-[-21deg] rounded-[30px] shadow-xl">
-              <img src="/images/e2.png" alt="" className="h-full w-full" />
-            </div>
-            <div className="absolute left-[58%] top-[3%] h-[314.39px] w-[313.54px] rotate-[-21deg] rounded-[30px] shadow-xl">
-              <img src="/images/e3.png" alt="" className="h-full w-full" />
-            </div>
-            <div className="absolute -top-[17%] left-[52%] h-[121.99px]  w-[249.58px] rotate-[-21deg] rounded-[30px] shadow-xl">
-              <img src="/images/e4.png" alt="" className="h-full w-full" />
-            </div>
-            <div className="absolute left-[46%] top-[55%] h-[231.47px] w-[445.52px] rotate-[-21deg] rounded-[30px] shadow-xl">
-              <img src="/images/e5.png" alt="" className="h-full w-full" />
-            </div>
+            <motion.div
+              initial={{ opacity: 0, x: -100, y: -75 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <div className="absolute -top-[10%] left-[4%] h-[198.06px] w-[367.09px] rotate-[-21deg] rounded-[35px] shadow-xl">
+                <img src="/images/e1.png" alt="" className="h-full w-full" />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -100, y: 135 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <div className="absolute left-[19%] top-[18%] h-[339.09px] w-[305.12px] rotate-[-21deg] rounded-[30px] shadow-xl">
+                <img src="/images/e2.png" alt="" className="h-full w-full" />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 100, y: 25 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <div className="absolute left-[58%] top-[3%] h-[314.39px] w-[313.54px] rotate-[-21deg] rounded-[30px] shadow-xl">
+                <img src="/images/e3.png" alt="" className="h-full w-full" />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 100, y: -125 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <div className="absolute -top-[17%] left-[52%] h-[121.99px]  w-[249.58px] rotate-[-21deg] rounded-[30px] shadow-xl">
+                <img src="/images/e4.png" alt="" className="h-full w-full" />
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 100, y: 405 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <div className="absolute left-[46%] top-[55%] h-[231.47px] w-[445.52px] rotate-[-21deg] rounded-[30px] shadow-xl">
+                <img src="/images/e5.png" alt="" className="h-full w-full" />
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
--- components/howWorkSection/index.tsx
+++ components/howWorkSection/index.tsx
@@ -1,5 +1,7 @@
+"use client";
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { motion } from "framer-motion";
 
 const appDesc = [
   {
@@ -22,7 +24,12 @@ const HowItWorkSection = () => {
       <div className="mx-auto max-w-full">
         <div className="grid grid-cols-hero gap-x-[76px]">
           <div className="flex flex-col gap-y-[37px]">
-            <div>
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+            >
               <h3 className="pb-[5px] font-jost text-head2 font-semibold">
                 How It Works
               </h3>
@@ -30,10 +37,18 @@ const HowItWorkSection = () => {
                 Unique and powerful suite of software to run your business,
                 brought to you by vision to you work.
               </p>
-            </div>
+            </motion.div>
             <div className="flex flex-col gap-y-[17px]">
               {appDesc.map((item, index) => (
-                <div
+                <motion.div
+                  initial={{ opacity: 0, x: -100 }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{
+                    duration: 0.5,
+                    ease: "easeInOut",
+                    delay: 0.5 + index * 0.1,
+                  }}
+                  viewport={{ once: true }}
                   key={index}
                   className="rounded-[30px] bg-gradient-to-r from-[#e0e0e0]/0 to-[#e0e0e0] p-px"
                 >
@@ -50,32 +65,56 @@ const HowItWorkSection = () => {
                       </p>
                     </div>
                   </div>
-                </div>
+                </motion.div>
               ))}
             </div>
           </div>
           <div className="mobile_app_section flex flex-col justify-center self-center rounded-l-[30px] bg-[#f6f6f6] pb-[85px] pl-[80px] pt-[115px]  ">
             <div className="max-w-[551px]">
-              <h3 className="pb-[21px] font-jost text-head2 font-semibold text-primary-03">
+              <motion.h3
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+                className="pb-[21px] font-jost text-head2 font-semibold text-primary-03"
+              >
                 The Perfect CRM Software: Boost Your Field Sales Team’s
                 Efficiency
-              </h3>
-              <p className="pb-[22px] font-open-sans text-head5 text-[#3ba3cb] ">
+              </motion.h3>
+              <motion.p
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+                className="pb-[22px] font-open-sans text-head5 text-[#3ba3cb] "
+              >
                 Depending on the Sales Approaches Standardize a Marketing
                 Process
-              </p>
-              <p className="font-open-sans text-head6 text-primary-03">
+              </motion.p>
+              <motion.p
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+                className="font-open-sans text-head6 text-primary-03"
+              >
                 Unique and powerful suite of software to run your entire
                 business, brought to you by a company.
-              </p>
-              <div className="mt-[20px] flex items-center gap-x-[26px]">
+              </motion.p>
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+                className="mt-[20px] flex items-center gap-x-[26px]"
+              >
                 <div>
                   <img src="/images/Apple.png" alt="" />
                 </div>
                 <div>
                   <img src="/images/Google.png" alt="" />
                 </div>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
